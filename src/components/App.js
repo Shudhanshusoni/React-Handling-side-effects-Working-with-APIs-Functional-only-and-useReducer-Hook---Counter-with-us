db61b7b752ec07d32195193adc83920c25988599
Shudhanshusoni <97749507+Shudhanshusoni@users.noreply.github.com>
2022-05-22 15:17:47 +0530

    c

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,17 @@ import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
 const App = () => {
 const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/0})
+function Increment(){
+  dispatch({type:'increment'})
+}
+function Decrement(){
+  dispatch({type:'decrement'})
+}
   return (
     <div id="main">
       <span id="counter">{state}</span>
-      <button id="increment-btn" onClick={()=>dispatch({type :"increment"})}></button>
-      <button id="decrement-btn" onClick={()=>dispatch({type :"decrement"})}></button>
+      <button id="increment-btn" onClick={Increment}></button>
+      <button id="decrement-btn" onClick={Decrement}></button>
     </div>
   )
 }
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,11 +1,11 @@
 const counterReducer = (state,action) =>{
 switch(action.type){
     case "increment":
-        return state+1;
+        return state+1
     case "decrement":
-        return state-1;
+        return state-1
         default:
-        return state;
+        return state
 }
 }
 
